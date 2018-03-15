c4bc3375ebedd545473dfd7ecc535e19ac81822c
Aashish095 <37073516+Aashish095@users.noreply.github.com>
2018-03-15 19:35:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,8 @@ var element = document.getElementById("main-text"
 );
 element.innerHTML ='NEW value';
 
+var img = document.getElementById("modi");
+img.onclick = function(){
+    img.style.marginleft='100px';
+
+};
