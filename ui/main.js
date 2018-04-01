b11d62953d07ce10d98bd2850767452d2d3ad9e7
Aashish095 <37073516+Aashish095@users.noreply.github.com>
2018-04-01 16:18:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element = document.getElementById("main-text"
 element.innerHTML ='aashish';
 
 var img = document.getElementById("madi");
-var marginleft =0;
+
 img.onclick = function(){
     img.style.marginleft = '100px';
 
