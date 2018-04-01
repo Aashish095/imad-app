aeffecc7e511849d5692d2b6584372a1695d9bf8
Aashish095 <37073516+Aashish095@users.noreply.github.com>
2018-04-01 16:17:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,12 +7,7 @@ element.innerHTML ='aashish';
 
 var img = document.getElementById("madi");
 var marginleft =0;
-function moveRight(){
-    marginleft= marginleft = 10;
-    img.style.marginleft = marginleft = 'px';
-}
 img.onclick = function(){
-    
-    var interval = setinterval(moveRight ,100);
+    img.style.marginleft = '100px';
 
 };
